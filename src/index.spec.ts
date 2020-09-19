93711a7c80704f0e38026822e2df289362e8e1dc
Sindre S. Kjær <sindre@sindrekjr.no>
2020-09-19 19:59:07 +0200

    feat(IndexResolverFunction): it is now possible to pass a function instead of a number as the second argument

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,35 +1,93 @@
 import reduceToChunks from './index';
 
-describe('reduceToChunks(array: any[], rule: number)', () => {
+describe('reduceToChunks(array: any[], chunkRule: number)', () => {
   it('should not throw', () => expect(() => reduceToChunks([])).not.toThrow());
 
   it.each([
     [
       ['One', 'Two', 'Three', 'Four'],
       undefined,
-      [['One'], ['Two'], ['Three'], ['Four']]
+      [['One'], ['Two'], ['Three'], ['Four']],
     ],
     [
       ['One', 'Two', 'Three', 'Four'],
       2,
-      [['One', 'Two'], ['Three', 'Four']]
+      [['One', 'Two'], ['Three', 'Four']],
     ],
     [
       ['One', 'Two', 'Three', 'Four'],
       3,
-      [['One', 'Two', 'Three'], ['Four']]
+      [['One', 'Two', 'Three'], ['Four']],
     ],
     [
       ['One', 'Two', 'Three', 'Four'],
       4,
-      [['One', 'Two', 'Three', 'Four']]
+      [['One', 'Two', 'Three', 'Four']],
     ],
     [
       ['One', 'Two', 'Three', 'Four'],
       5,
-      [['One', 'Two', 'Three', 'Four']]
+      [['One', 'Two', 'Three', 'Four']],
     ],
-  ])('should chunk correctly', (array, chunkSize, expected) => {
-    expect(reduceToChunks(array, chunkSize)).toEqual(expected);
+  ])('should chunk correctly with sizes', (array, size, expected) => {
+    expect(reduceToChunks(array, size)).toEqual(expected);
+  });
+});
+
+describe('reduceToChunks(array: any[], chunkRule: Function)', () => {
+  it.each([
+    [
+      ['One', 'Two', 'Three', 'Four'],
+      (val: string) => val ? 0 : 1,
+      [['One', 'Two', 'Three', 'Four']],
+    ],
+    [
+      ['One', 'Two', 'Three', 'Four'],
+      (val: string) => val === 'One' ? 0 : 1,
+      [['One'], ['Two', 'Three', 'Four']],
+    ],
+    [
+      ['One', 'Two', 'Three', 'Four'],
+      (val: string) => val.length,
+      [['One', 'Two'], ['Four'], ['Three']],
+    ],
+  ])('should chunk correctly with functions that examine the value', (array, func, expected) => {
+    expect(reduceToChunks(array, func)).toEqual(expected);
+  });
+
+  it.each([
+    [
+      ['One', 'Two', 'Three', 'Four'],
+      (val: string, index: number) => index,
+      [['One'], ['Two'], ['Three'], ['Four']],
+    ],
+    [
+      ['One', 'Two', 'Three', 'Four'],
+      (val: string, index: number) => index / 2,
+      [['One'], ['Three']],
+    ],
+    [
+      ['One', 'Two', 'Three', 'Four'],
+      (val: string, index: number) => index * 2,
+      [['One'], ['Two'], ['Three'], ['Four']],
+    ],
+  ])('should chunk correctly with functions that examine the index', (array, func, expected) => {
+    expect(reduceToChunks(array, func)).toEqual(expected);
+  });
+
+  it.each([
+    [
+      ['One', 'Two', 'Three', 'Four'],
+      (val: string) => val.length,
+      [undefined, undefined, undefined, ['One', 'Two'], ['Four'], ['Three']],
+    ],
+    [
+      ['One', 'Two', 'Three', 'Four'],
+      (val: string, index: number) => index * 2,
+      [['One'], undefined, ['Two'], undefined, ['Three'], undefined, ['Four']],
+    ],
+  ])('should keep undefined when preserveEmptyEntries is set to true', (array, func, expected) => {
+    console.log(reduceToChunks(array, func, true));
+    expect(reduceToChunks(array, func, true)).toEqual(expected);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-export default function reduceToChunks(array: any[], chunkSize: number = 1): Array<any[]> {
-  return chunkBySize(array, chunkSize);
+type IndexResolverFunction = {
+  (item: any, index: number): number;
 }
 
-const chunkBySize = (array: any[], chunkSizes: number): Array<any[]> => array.reduce((acc: Array<any[]>, item: any, index: number) => {
-  acc[Math.floor(index / chunkSizes)].push(item);
+export default function reduceToChunks(array: any[], chunkSize?: number): Array<any[]>;
+export default function reduceToChunks(array: any[], chunkFunc: IndexResolverFunction, preserveEmptyEntries?: boolean): Array<any[]>;
+export default function reduceToChunks(array: any[], chunkRule: IndexResolverFunction | number = 1, preserveEmptyEntries?: boolean): Array<any[]> {
+  if (preserveEmptyEntries) {
+    return typeof chunkRule === 'number' ? chunkBySize(array, chunkRule) : chunkByFunction(array, chunkRule);
+  }
+  return typeof chunkRule === 'number' ? chunkBySize(array, chunkRule) : chunkByFunction(array, chunkRule).filter(entry => entry);
+}
+
+const chunkBySize = (array: any[], size: number): Array<any[]> => array.reduce((acc: Array<any[]>, item: any, index: number) => {
+  acc[Math.floor(index / size)].push(item);
   return acc;
-}, Array.from(Array(Math.ceil(array.length / chunkSizes)), () => []));
+}, Array.from(Array(Math.ceil(array.length / size)), () => []));
+
+const chunkByFunction = (array: any[], func: IndexResolverFunction): Array<any[]> => array.reduce((acc: Array<any[]>, item: any, index: number) => {
+  const resolvedIndex = func(item, index);
+  if (acc[resolvedIndex]) {
+    acc[resolvedIndex].push(item);
+  } else {
+    if (wholeNumber(resolvedIndex)) acc[resolvedIndex] = [item];
+  }
+  return acc;
+}, Array<any[]>());
+
+const wholeNumber = (n: number) => (n - Math.floor(n)) === 0;
 
 module.exports = reduceToChunks;
