3fe34cea39c83c992de04eaca23cf4500559df80
Sindre S. Kjær <sindre@sindrekjr.no>
2020-09-20 16:21:50 +0200

    feat: allow chunkFunc to return undefined, in which case item is filtered out

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,21 @@ const chunkBySize = (array: any[], size: number): Array<any[]> => array.reduce((
   return acc;
 }, Array.from(Array(Math.ceil(array.length / size)), () => []));
 
-const chunkByFunction = (array: any[], func: (item: any, index: number) => number): Array<any[]> => array.reduce((acc: Array<any[]>, item: any, index: number) => {
+const chunkByFunction = (array: any[], func: (item: any, index: number) => number | undefined): Array<any[]> => array.reduce((acc: Array<any[]>, item: any, index: number) => {
   const resolvedIndex = func(item, index);
-  if (acc[resolvedIndex]) {
-    acc[resolvedIndex].push(item);
-  } else if (resolvedIndex - Math.floor(resolvedIndex) === 0) {
-    acc[resolvedIndex] = [item];
+  if (resolvedIndex != undefined) {
+    if (acc[resolvedIndex]) {
+      acc[resolvedIndex].push(item);
+    } else if (resolvedIndex - Math.floor(resolvedIndex) === 0) {
+      acc[resolvedIndex] = [item];
+    }
   }
   return acc;
 }, []);
 
 export default function reduceToChunks(array: any[], chunkSize?: number): Array<any[]>;
-export default function reduceToChunks(array: any[], chunkFunc: (item: any, index: number) => number, preserveEmptyEntries?: boolean): Array<any[]>;
-export default function reduceToChunks(array: any[], chunkRule: ((item: any, index: number) => number) | number = 1, preserveEmptyEntries?: boolean): Array<any[]> {
+export default function reduceToChunks(array: any[], chunkFunc: (item: any, index: number) => number | undefined, preserveEmptyEntries?: boolean): Array<any[]>;
+export default function reduceToChunks(array: any[], chunkRule: ((item: any, index: number) => number | undefined) | number = 1, preserveEmptyEntries?: boolean): Array<any[]> {
   if (preserveEmptyEntries) {
     return typeof chunkRule === 'number' ? chunkBySize(array, chunkRule) : chunkByFunction(array, chunkRule);
   }
