a943746486463f9af6ec39cf7c675b419685cec4
Sindre S. Kjær <sindre@sindrekjr.no>
2020-09-20 15:47:22 +0200

    chore: cleanup index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,25 @@
-type IndexResolverFunction = {
-  (item: any, index: number): number;
-}
-
-export default function reduceToChunks(array: any[], chunkSize?: number): Array<any[]>;
-export default function reduceToChunks(array: any[], chunkFunc: IndexResolverFunction, preserveEmptyEntries?: boolean): Array<any[]>;
-export default function reduceToChunks(array: any[], chunkRule: IndexResolverFunction | number = 1, preserveEmptyEntries?: boolean): Array<any[]> {
-  if (preserveEmptyEntries) {
-    return typeof chunkRule === 'number' ? chunkBySize(array, chunkRule) : chunkByFunction(array, chunkRule);
-  }
-  return typeof chunkRule === 'number' ? chunkBySize(array, chunkRule) : chunkByFunction(array, chunkRule).filter(entry => entry);
-}
-
 const chunkBySize = (array: any[], size: number): Array<any[]> => array.reduce((acc: Array<any[]>, item: any, index: number) => {
   acc[Math.floor(index / size)].push(item);
   return acc;
 }, Array.from(Array(Math.ceil(array.length / size)), () => []));
 
-const chunkByFunction = (array: any[], func: IndexResolverFunction): Array<any[]> => array.reduce((acc: Array<any[]>, item: any, index: number) => {
+const chunkByFunction = (array: any[], func: (item: any, index: number) => number): Array<any[]> => array.reduce((acc: Array<any[]>, item: any, index: number) => {
   const resolvedIndex = func(item, index);
   if (acc[resolvedIndex]) {
     acc[resolvedIndex].push(item);
-  } else {
-    if (wholeNumber(resolvedIndex)) acc[resolvedIndex] = [item];
+  } else if (resolvedIndex - Math.floor(resolvedIndex) === 0) {
+    acc[resolvedIndex] = [item];
   }
   return acc;
-}, Array<any[]>());
+}, []);
 
-const wholeNumber = (n: number) => (n - Math.floor(n)) === 0;
+export default function reduceToChunks(array: any[], chunkSize?: number): Array<any[]>;
+export default function reduceToChunks(array: any[], chunkFunc: (item: any, index: number) => number, preserveEmptyEntries?: boolean): Array<any[]>;
+export default function reduceToChunks(array: any[], chunkRule: ((item: any, index: number) => number) | number = 1, preserveEmptyEntries?: boolean): Array<any[]> {
+  if (preserveEmptyEntries) {
+    return typeof chunkRule === 'number' ? chunkBySize(array, chunkRule) : chunkByFunction(array, chunkRule);
+  }
+  return typeof chunkRule === 'number' ? chunkBySize(array, chunkRule) : chunkByFunction(array, chunkRule).filter(entry => entry);
+}
 
 module.exports = reduceToChunks;
