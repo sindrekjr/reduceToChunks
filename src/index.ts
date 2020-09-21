a91e508be108875155901205808b45b343b4ef64
Sindre S. Kjær <sindre@sindrekjr.no>
2020-09-21 17:31:52 +0200

    feat: greatly improved efficiency by removing several unnecessary iterations

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 const chunkBySize = <T>(array: T[], size: number): Array<T[]> => array.reduce((acc: Array<T[]>, item: T, index: number) => {
-  acc[Math.floor(index / size)].push(item);
+  const chunkIndex = Math.floor(index / size);
+  if (acc[chunkIndex]) {
+    acc[chunkIndex].push(item);
+  } else {
+    acc[chunkIndex] = [item];
+  }
   return acc;
-}, Array.from(Array(Math.ceil(array.length / size)), () => []));
+}, []);
 
 const chunkByFunction = <T>(array: T[], func: (item: T, index: number) => number | undefined): Array<T[]> => array.reduce((acc: Array<T[]>, item: T, index: number) => {
-  const resolvedIndex = func(item, index);
-  if (resolvedIndex !== undefined) {
-    if (acc[resolvedIndex]) {
-      acc[resolvedIndex].push(item);
-    } else if (resolvedIndex - Math.floor(resolvedIndex) === 0) {
-      acc[resolvedIndex] = [item];
+  const chunkIndex = func(item, index);
+  if (chunkIndex !== undefined) {
+    if (acc[chunkIndex]) {
+      acc[chunkIndex].push(item);
+    } else if (chunkIndex - Math.floor(chunkIndex) === 0) {
+      acc[chunkIndex] = [item];
     }
   }
   return acc;
@@ -18,7 +23,7 @@ const chunkByFunction = <T>(array: T[], func: (item: T, index: number) => number
 export default function reduceToChunks<T>(array: T[], chunkSize?: number): Array<T[]>;
 export default function reduceToChunks<T>(array: T[], chunkFunc: (item: T, index: number) => number | undefined, preserveEmptyEntries?: boolean): Array<T[]>;
 export default function reduceToChunks<T>(array: T[], chunkRule: ((item: T, index: number) => number | undefined) | number = 1, preserveEmptyEntries?: boolean): Array<T[]> {
-  if (preserveEmptyEntries) {
+  if (preserveEmptyEntries || typeof chunkRule === 'number') {
     return typeof chunkRule === 'number' ? chunkBySize<T>(array, chunkRule) : chunkByFunction<T>(array, chunkRule);
   }
   return typeof chunkRule === 'number' ? chunkBySize<T>(array, chunkRule) : chunkByFunction<T>(array, chunkRule).filter(entry => entry);
